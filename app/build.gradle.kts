f252d17fcfa338a5f9f334ee8c3e7101572c1ce7
M_K_A <jmikha381@gmail.com>
2024-08-18 00:07:15 +0530

    Upgrade the compile and target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dw.aiassist"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dw.aiassist"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
